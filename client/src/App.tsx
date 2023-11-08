06c36ae6d19a138d7d4056417ff7523c4836d800
Great Ogheneochuko <greatochuko123@gmail.com>
2023-11-08 16:41:22 +0100

    create router App layout and home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
 
 function App() {
-  return <h1>App</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
