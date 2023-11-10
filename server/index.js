c1e1673c6f6410edfea935a7865a64c17755ae91
Great Ogheneochuko <greatochuko123@gmail.com>
2023-11-10 12:42:41 +0100

    connect to local mongoose database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 import authRouter from "./routes/authRoutes.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRouter);
 
-app.listen(PORT, () => {
-  console.log(`App running at port ${PORT}`);
-});
+async function connectToServer() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/web-dev-stories");
+    app.listen(PORT, () => {
+      console.log(`App running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+await connectToServer();
