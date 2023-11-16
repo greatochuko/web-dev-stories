27c1865d318d07a01b5644250079ceae1a419c8b
Great Ogheneochuko <greatochuko123@gmail.com>
2023-11-16 01:10:39 +0100

    add featured post carousel functionality

--- client/src/components/Featured.tsx
+++ client/src/components/Featured.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { fetchPosts } from "../services/postServices";
 import { PostType } from "./Post";
+import FeaturedPost from "./FeaturedPost";
 
 export default function Featured() {
   const [posts, setPost] = useState<PostType[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  console.log(currentIndex);
 
   useEffect(() => {
     async function getPosts() {
@@ -13,37 +16,40 @@ export default function Featured() {
     }
     getPosts();
   }, []);
+
+  function increaseIndex() {
+    setCurrentIndex((curr) => {
+      if (curr >= 2) return 0;
+      return curr + 1;
+    });
+  }
+
+  function decreaseIndex() {
+    setCurrentIndex((curr) => {
+      if (curr <= 0) return 2;
+      return curr - 1;
+    });
+  }
   return (
     <section className="w-full py-4 px-[5%] md:px-[10%] text-zinc-700 mb-8">
       <div className="gap-2 flex relative aspect-[1.5] md:aspect-[2] max-w-7xl mx-auto ">
-        {posts.slice(0, 3).map((post) => (
-          <Link
-            to={`/post/${post._id}`}
+        {posts.slice(0, 3).map((post, index) => (
+          <FeaturedPost
             key={post._id}
-            className="bg-black h-full w-full top-0 left-0 absolute overflow-hidden rounded-md"
-          >
-            <img
-              src={post.banner}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            <div className="flex flex-col gap-2 text-white bg-black/50 absolute top-0 left-0 w-full h-full px-8 md:px-12 py-4 md:py-12 justify-end">
-              <p>{post.category}</p>
-              <h2 className="text-base md:text-2xl font-semibold">
-                {post.title}
-              </h2>
-              <p>{new Date(post.createdAt).toDateString()}</p>
-              <div className="flex gap-2 items-center">
-                <div className="w-10 aspect-square bg-zinc-500 rounded-full"></div>
-                <h3>{post.author.fullName}</h3>
-              </div>
-            </div>
-          </Link>
+            post={post}
+            hidden={!(index === currentIndex)}
+          />
         ))}
-        <button className="text-zinc-300 hover:text-white absolute left-0 h-full flex items-center justify-center w-8 md:w-12 from-black/50 to-transparent text-2xl font-bold hover:bg-gradient-to-r ">
+        <button
+          onClick={decreaseIndex}
+          className="text-zinc-300 hover:text-white absolute left-0 h-full flex items-center justify-center w-8 md:w-12 from-black/50 to-transparent text-2xl font-bold hover:bg-gradient-to-r "
+        >
           <i className="fa-solid fa-caret-left"></i>
         </button>
-        <button className="text-zinc-300 hover:text-white absolute right-0 h-full flex items-center justify-center w-8 md:w-12 from-black/50 to-transparent text-2xl font-bold hover:bg-gradient-to-l ">
+        <button
+          onClick={increaseIndex}
+          className="text-zinc-300 hover:text-white absolute right-0 h-full flex items-center justify-center w-8 md:w-12 from-black/50 to-transparent text-2xl font-bold hover:bg-gradient-to-l "
+        >
           <i className="fa-solid fa-caret-right"></i>
         </button>
       </div>
---
+++ client/src/components/FeaturedPost.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { PostType } from "./Post";
+
+export default function FeaturedPost({
+  post,
+  hidden,
+}: {
+  post: PostType;
+  hidden?: boolean;
+}) {
+  return (
+    <Link
+      to={`/post/${post._id}`}
+      key={post._id}
+      className={`bg-black h-full w-full top-0 left-0 absolute overflow-hidden rounded-md duration-200 ${
+        hidden ? "opacity-0" : ""
+      }`}
+    >
+      <img src={post.banner} alt="" className="w-full h-full object-cover" />
+      <div className="flex flex-col gap-2 text-white bg-black/50 absolute top-0 left-0 w-full h-full px-8 md:px-12 py-4 md:py-12 justify-end">
+        <p>{post.category}</p>
+        <h2 className="text-base md:text-2xl font-semibold">{post.title}</h2>
+        <p>{new Date(post.createdAt).toDateString()}</p>
+        <div className="flex gap-2 items-center">
+          <div className="w-10 aspect-square bg-zinc-500 rounded-full"></div>
+          <h3>{post.author.fullName}</h3>
+        </div>
+      </div>
+    </Link>
+  );
+}
